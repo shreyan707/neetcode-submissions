d294aafe85886079b54fb2d5e1f1fe38e12c7343
Shreyan <139256319+shreyan707@users.noreply.github.com>
2026-07-23 22:54:59 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) 
+    {
+        Arrays.sort(nums);
+        backtrack(nums, 0, new ArrayList<>());
+        return ans;
+    }
+
+    public void backtrack(int[] arr, int index, List<Integer> current)
+    {
+       // System.out.println("\n\nINDEX: "+index);
+        //System.out.println("Current: "+current);
+        //System.out.println("BASE CASE: "+current);
+        if(!ans.contains(current))
+            ans.add(new ArrayList(current));
+
+
+        for(int i=index;i<arr.length; i++)
+        {
+            //System.out.println("i : "+i);
+            if(i>index && arr[i]==arr[i-1]) continue;
+            current.add(arr[i]);
+            backtrack(arr,i+1, current);
+            current.remove(current.size()-1);
+            //System.out.println("BACKTRACK : "+current);
+        }
+
+    }
+}
